13c2568da9ebeab1a7f1b6e108f71dea464de7ca
ExcellentPig <58382766+ExcellentPig@users.noreply.github.com>
2020-04-10 16:15:44 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <ul>
+    	<li v-for="(user) in users" :key="user.id">
+			{{user.name}}
+		</li>
+    </ul>
   </div>
 </template>
-
+<script>
+import axios from 'axios'
+	export default{
+		name:'app',
+		data(){
+			return {
+				users:[]
+			}
+		},
+		mounted(){
+			axios.get('http://localhost:3000/api/users')
+				.then(response=>{
+					this.users = response.data;
+				})
+		}
+	}
+	
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
